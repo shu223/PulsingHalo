3d82e51796b12675b1465102fa67d320cc0ae1da
shu223 <shuichi0526@gmail.com>
2013-12-05 18:40:23 +0900

    first commit

---
+++ PulsingHalo/PulsingHaloLayer.h
@@ -0,0 +1,20 @@
+//
+//  PulsingLayer.h
+//  BeaconAnimationDemo
+//
+//  Created by shuichi on 12/5/13.
+//  Copyright (c) 2013 Shuichi Tsutsumi. All rights reserved.
+//
+//  Inspired by https://github.com/samvermette/SVPulsingAnnotationView
+
+
+#import <QuartzCore/QuartzCore.h>
+
+
+@interface PulsingHaloLayer : CALayer
+
+@property (nonatomic, assign) CGFloat radius;                   // default:60pt
+@property (nonatomic, assign) NSTimeInterval animationDuration; // default:3s
+@property (nonatomic, assign) NSTimeInterval pulseInterval; // default is 0s
+
+@end
--- PulsingHaloDemo/ViewController.m
+++ PulsingHaloDemo/ViewController.m
@@ -7,23 +7,81 @@
 //
 
 #import "ViewController.h"
+#import "PulsingHaloLayer.h"
+
+#define kMaxRadius 160
+
 
 @interface ViewController ()
-
+@property (nonatomic, strong) PulsingHaloLayer *halo;
+@property (nonatomic, weak) IBOutlet UIImageView *beaconView;
+@property (nonatomic, weak) IBOutlet UISlider *radiusSlider;
+@property (nonatomic, weak) IBOutlet UISlider *rSlider;
+@property (nonatomic, weak) IBOutlet UISlider *gSlider;
+@property (nonatomic, weak) IBOutlet UISlider *bSlider;
+@property (nonatomic, weak) IBOutlet UILabel *radiusLabel;
+@property (nonatomic, weak) IBOutlet UILabel *rLabel;
+@property (nonatomic, weak) IBOutlet UILabel *gLabel;
+@property (nonatomic, weak) IBOutlet UILabel *bLabel;
 @end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    self.halo = [PulsingHaloLayer layer];
+    self.halo.position = self.beaconView.center;
+    [self.view.layer insertSublayer:self.halo below:self.beaconView.layer];
+    
+    [self setupInitialValues];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+// =============================================================================
+#pragma mark - Private
+
+- (void)setupInitialValues {
+
+    self.radiusSlider.value = 0.5;
+    [self radiusChanged:nil];
+    
+    self.rSlider.value = 0;
+    self.gSlider.value = 0.487;
+    self.bSlider.value = 1.0;
+    [self colorChanged:nil];
+}
+
+
+// =============================================================================
+#pragma mark - IBAction
+
+- (IBAction)radiusChanged:(UISlider *)sender {
+    
+    self.halo.radius = self.radiusSlider.value * kMaxRadius;
+    self.radiusLabel.text = [@(self.radiusSlider.value) stringValue];
+}
+
+- (IBAction)colorChanged:(UISlider *)sender {
+    
+    UIColor *color = [UIColor colorWithRed:self.rSlider.value
+                                     green:self.gSlider.value
+                                      blue:self.bSlider.value
+                                     alpha:1.0];
+    
+    self.halo.backgroundColor = color.CGColor;
+    
+    self.rLabel.text = [@(self.rSlider.value) stringValue];
+    self.gLabel.text = [@(self.gSlider.value) stringValue];
+    self.bLabel.text = [@(self.bSlider.value) stringValue];
 }
 
 @end
