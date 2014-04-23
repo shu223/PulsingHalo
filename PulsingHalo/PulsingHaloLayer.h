56b459b6d24bc38fd0bc73b52d78d413e23025a8
Rune Madsen <runemadsen@mac.com>
2014-04-23 16:05:53 -0400

    repeatCount property

--- PulsingHalo/PulsingHaloLayer.h
+++ PulsingHalo/PulsingHaloLayer.h
@@ -13,8 +13,9 @@
 
 @interface PulsingHaloLayer : CALayer
 
-@property (nonatomic, assign) CGFloat radius;                   // default:60pt
-@property (nonatomic, assign) NSTimeInterval animationDuration; // default:3s
-@property (nonatomic, assign) NSTimeInterval pulseInterval; // default is 0s
+@property (nonatomic, assign) CGFloat radius;                   // default: 60pt
+@property (nonatomic, assign) NSTimeInterval animationDuration; // default: 3s
+@property (nonatomic, assign) NSTimeInterval pulseInterval;     // default: 0s
+@property (nonatomic, assign) CGFloat repeatCount;              // default: INFINITY
 
 @end
