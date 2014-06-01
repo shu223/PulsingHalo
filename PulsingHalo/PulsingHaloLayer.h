212fb7bcfb968f44ddc1ed596d2820fa3490dda3
shu223 <shuichi0526@gmail.com>
2014-06-01 14:26:04 -0700

    Added some properties to customize the animation

--- PulsingHalo/PulsingHaloLayer.h
+++ PulsingHalo/PulsingHaloLayer.h
@@ -14,6 +14,9 @@
 @interface PulsingHaloLayer : CALayer
 
 @property (nonatomic, assign) CGFloat radius;                   // default: 60pt
+@property (nonatomic, assign) CGFloat fromValueForRadius;       // default: 0.0
+@property (nonatomic, assign) CGFloat fromValueForAlpha;        // default: 0.45
+@property (nonatomic, assign) CGFloat keyTimeForHalfOpacity;    // default: 0.2 (range: 0 < keyTime < 1)
 @property (nonatomic, assign) NSTimeInterval animationDuration; // default: 3s
 @property (nonatomic, assign) NSTimeInterval pulseInterval;     // default: 0s
 @property (nonatomic, assign) float repeatCount;                // default: INFINITY
--- PulsingHalo/PulsingHaloLayer.m
+++ PulsingHalo/PulsingHaloLayer.m
@@ -18,35 +18,38 @@
 
 @implementation PulsingHaloLayer
 
-- (id)initWithRepeatCount:(float)repeatCount
+- (id)initWithRepeatCount:(float) repeatCount
 {
-  self = [super init];
-  if (self) {
-    self.contentsScale = [UIScreen mainScreen].scale;
-    self.opacity = 0;
-  
-    // default
-    self.radius = 60;
-    self.animationDuration = 3;
-    self.pulseInterval = 0;
-    self.repeatCount = repeatCount;
-    self.backgroundColor = [[UIColor colorWithRed:0.000 green:0.478 blue:1.000 alpha:1] CGColor];
-
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void) {
-      
-        [self setupAnimationGroup];
-
-        if(self.pulseInterval != INFINITY) {
-          
-            dispatch_async(dispatch_get_main_queue(), ^(void) {
-              
-                [self addAnimation:self.animationGroup forKey:@"pulse"];
-            });
-        }
-    });
-  }
-  return self;
-  
+    self = [super init];
+    if (self) {
+        self.contentsScale = [UIScreen mainScreen].scale;
+        self.opacity = 0;
+        
+        // default
+        self.radius = 60;
+        self.fromValueForRadius = 0.0;
+        self.fromValueForAlpha = 0.45;
+        self.keyTimeForHalfOpacity = 0.2;
+        self.animationDuration = 3;
+        self.pulseInterval = 0;
+        self.repeatCount = repeatCount;
+        self.backgroundColor = [[UIColor colorWithRed:0.000 green:0.478 blue:1.000 alpha:1] CGColor];
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void) {
+            
+            [self setupAnimationGroup];
+            
+            if(self.pulseInterval != INFINITY) {
+                
+                dispatch_async(dispatch_get_main_queue(), ^(void) {
+                    
+                    [self addAnimation:self.animationGroup forKey:@"pulse"];
+                });
+            }
+        });
+    }
+    return self;
+    
 }
 
 - (id)init {
@@ -54,13 +57,13 @@
 }
 
 - (void)setRadius:(CGFloat)radius {
-
+    
     _radius = radius;
     
     CGPoint tempPos = self.position;
-
+    
     CGFloat diameter = self.radius * 2;
-
+    
     self.bounds = CGRectMake(0, 0, diameter, diameter);
     self.cornerRadius = self.radius;
     self.position = tempPos;
@@ -77,14 +80,14 @@
     self.animationGroup.timingFunction = defaultCurve;
     
     CABasicAnimation *scaleAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale.xy"];
-    scaleAnimation.fromValue = @0.0;
+    scaleAnimation.fromValue = @(self.fromValueForRadius);
     scaleAnimation.toValue = @1.0;
     scaleAnimation.duration = self.animationDuration;
     
     CAKeyframeAnimation *opacityAnimation = [CAKeyframeAnimation animationWithKeyPath:@"opacity"];
     opacityAnimation.duration = self.animationDuration;
-    opacityAnimation.values = @[@0.45, @0.45, @0];
-    opacityAnimation.keyTimes = @[@0, @0.2, @1];
+    opacityAnimation.values = @[@(self.fromValueForAlpha), @0.45, @0];
+    opacityAnimation.keyTimes = @[@0, @(self.keyTimeForHalfOpacity), @1];
     opacityAnimation.removedOnCompletion = NO;
     
     NSArray *animations = @[scaleAnimation, opacityAnimation];
