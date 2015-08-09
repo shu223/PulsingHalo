17082b7940d5019f77553ee85411ce3c9e79f31e
Andrew Breckenridge <asbreckenridge@me.com>
2015-08-08 19:31:15 -0700

    created shared framework

---
+++ PulsingHalo/PulsingHalo.h
@@ -0,0 +1,19 @@
+//
+//  PulsingHalo.h
+//  PulsingHalo
+//
+//  Created by Andrew Breckenridge on 8/8/15.
+//  Copyright © 2015 Andrew Breckenridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PulsingHalo.
+FOUNDATION_EXPORT double PulsingHaloVersionNumber;
+
+//! Project version string for PulsingHalo.
+FOUNDATION_EXPORT const unsigned char PulsingHaloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PulsingHalo/PublicHeader.h>
+
+
