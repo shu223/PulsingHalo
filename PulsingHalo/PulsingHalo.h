807f317776969bae7c78c1f57d0326169746c748
shu223 <shuichi0526@gmail.com>
2016-03-11 22:46:35 +0900

    Create shared framework

---
+++ PulsingHalo/PulsingHalo.h
@@ -0,0 +1,20 @@
+//
+//  PulsingHalo.h
+//  PulsingHalo
+//
+//  Created by Shuichi Tsutsumi on 3/11/16.
+//  Copyright © 2016 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PulsingHalo.
+FOUNDATION_EXPORT double PulsingHaloVersionNumber;
+
+//! Project version string for PulsingHalo.
+FOUNDATION_EXPORT const unsigned char PulsingHaloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PulsingHalo/PublicHeader.h>
+
+#import <PulsingHalo/PulsingHaloLayer.h>
+
