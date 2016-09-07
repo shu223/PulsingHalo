80ac3fbcc4beb096818b86418dd458bcd40b7815
shu223 <shuichi0526@gmail.com>
2016-09-07 11:34:58 +0900

    http://stackoverflow.com/questions/19789958/xcode-preprocessor-macro-to-check-if-base-sdk-ios-7-0

--- PulsingHalo/PulsingHaloLayer.m
+++ PulsingHalo/PulsingHaloLayer.m
@@ -10,7 +10,7 @@
 
 #import "PulsingHaloLayer.h"
 
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 100000
 @interface PulsingHaloLayer () <CAAnimationDelegate>
 #else
 @interface PulsingHaloLayer ()
