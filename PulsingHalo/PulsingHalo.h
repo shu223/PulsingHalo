d2a273d5ca0bd1aca360cec06ec5e1745e94b2f4
Zero.D.Saber <fuxianchao@gmail.com>
2021-10-12 13:51:56 +0800

    fix misjudgment import

--- PulsingHalo/PulsingHalo.h
+++ PulsingHalo/PulsingHalo.h
@@ -17,8 +17,9 @@ FOUNDATION_EXPORT const unsigned char PulsingHaloVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <PulsingHalo/PublicHeader.h>
 
 #if __has_include(<PulsingHalo/PulsingHaloLayer.h>)
-#else
 #import <PulsingHalo/PulsingHaloLayer.h>
+#else
+#import "PulsingHaloLayer.h"
 #endif
 
 
